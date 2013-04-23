1b645efa91e235699268848f15f1b075379d3795
Matthew Colson <colson78@live.missouristate.edu>
2013-04-23 10:12:55 -0500

    up to apr23

--- app/controllers/honored_organizations_controller.rb
+++ app/controllers/honored_organizations_controller.rb
@@ -1,9 +1,12 @@
 class HonoredOrganizationsController < ApplicationController
   def new
     @organization = Organization.all
+    @honored_organization = HonoredOrganization.new
+    @organization_id = []
   end
 
   def create
+    
   end
 
   def update
